035d70f0de9ac1bbedcbc1eea90c935d907eb452
root <root@yabad.42.fr>
2023-10-19 17:24:11 -0400

    modify nginx configuration files to fit wordpress configuration

--- srcs/requirements/nginx/tools/nginx_conf.sh
+++ srcs/requirements/nginx/tools/nginx_conf.sh
@@ -5,7 +5,7 @@ mkdir -p /etc/nginx/ssl
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
 	-keyout /etc/nginx/ssl/yabad.key \
     -out /etc/nginx/ssl/yabad.crt  \
-    -subj "/C=MA/L=Martil/O=42/OU=1337 MED/CN=yabad.42.fr" &>/dev/null
+    -subj "/C=MA/L=Martil/O=42/OU=1337 MED/CN=yabad.42.fr"
 
 # [ starting nginx process ]
 nginx -g "daemon off;"
