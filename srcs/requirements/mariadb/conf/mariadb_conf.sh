57eab839cd99c6e3743d790e017b4fa1288ed49d
root <root@yabad.42.fr>
2023-10-19 17:27:04 -0400

    making the database server accessible by USER from any remote host in the network

--- srcs/requirements/mariadb/conf/mariadb_conf.sh
+++ srcs/requirements/mariadb/conf/mariadb_conf.sh
@@ -18,17 +18,15 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 USE mysql;
 FLUSH PRIVILEGES;
 DELETE FROM mysql.user WHERE User='';
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';
 CREATE DATABASE $WP_DB_NAME;
-CREATE USER '$WP_DB_USER'@'localhost' IDENTIFIED BY '$WP_DB_PASS';
-GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$WP_DB_USER'@'localhost';
+CREATE USER '$WP_DB_USER'@'%' IDENTIFIED BY '$WP_DB_PASS';
+GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$WP_DB_USER'@'%';
 FLUSH PRIVILEGES;
 EOF
 	mariadbd --user=mysql --bootstrap < $file
 	rm -f $file
 fi
 
-exec /usr/bin/mysqld --user=mysql --bind-address=0.0.0.0
+exec /usr/bin/mysqld --user=mysql --console
