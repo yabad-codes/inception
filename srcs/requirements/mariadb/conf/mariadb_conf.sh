fed8bd211ddc1afc17fbb1525aff6575eaf9ed68
root <root@yabad.42.fr>
2023-10-20 17:19:02 -0400

    making the logs more structured and readable

--- srcs/requirements/mariadb/conf/mariadb_conf.sh
+++ srcs/requirements/mariadb/conf/mariadb_conf.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
 if [ ! -d "/run/mysqld" ]; then
+	echo "[ mariadb ] creating mysqld folder..."
 	mkdir -p /run/mysqld
 	chown -R mysql:mysql /run/mysqld
 fi
 
+echo "[ mariadb ] enabling network access to mariadb server..."
 sed -i "s|skip-networking|#skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 sed -i "s|#bind-address=0.0.0.0|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
 	chown -R mysql:mysql /var/lib/mysql
 	
+	echo "[ mariadb ] mariadb installation..."
 	mariadb-install-db --datadir=/var/lib/mysql --user=mysql --skip-test-db
 
+	echo "[ mariadb ] configuring mariadb..."
 	file=/tmp/.tmp
 	cat << EOF > $file
 USE mysql;
@@ -29,4 +33,5 @@ EOF
 	rm -f $file
 fi
 
-exec /usr/bin/mysqld --user=mysql --console
+echo "[ mariadb ] starting mariadb in the foreground..."
+exec /usr/bin/mysqld --user=mysql --console &>/dev/null
--- srcs/requirements/nginx/tools/nginx_conf.sh
+++ srcs/requirements/nginx/tools/nginx_conf.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 # [ generating ssl certificates ]
+echo "[ nginx ] generating ssl certificate..."
 mkdir -p /etc/nginx/ssl
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
 	-keyout /etc/nginx/ssl/yabad.key \
     -out /etc/nginx/ssl/yabad.crt  \
-    -subj "/C=MA/L=Martil/O=42/OU=1337 MED/CN=yabad.42.fr"
-
+    -subj "/C=MA/L=Martil/O=42/OU=1337 MED/CN=yabad.42.fr" &>/dev/null
+echo "[ nginx ] a private key and certificate generated successfully."
 # [ starting nginx process ]
+echo "[ nginx ] starting nginx in the foreground..."
 nginx -g "daemon off;"
