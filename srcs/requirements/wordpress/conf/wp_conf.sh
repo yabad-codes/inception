fdbaaf05d02b4573704f66d5de76a1c2c43b1f90
yabad <yahya.abad9@gmail.com>
2023-10-27 14:51:25 +0100

    redis setup with wordpress

--- srcs/requirements/wordpress/conf/wp_conf.sh
+++ srcs/requirements/wordpress/conf/wp_conf.sh
@@ -6,6 +6,8 @@ do
 	sleep 3
 done
 
+chmod -R 755 /var/www/html/wordpress
+
 if [ ! -f ${WP_PATH}/wp-config.php ]
 then
 	echo "[ wordpress ] installing..."
@@ -14,9 +16,22 @@ then
 	wp config create --dbname=${WP_DB_NAME} --dbuser=${WP_DB_USER} --dbpass=${WP_DB_PASS} --dbhost=${DB_HOST} --path=${WP_PATH} --allow-root
 	wp core install --url=${NGINX_HOST} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_EMAIL} --path=${WP_PATH} --allow-root
 	wp user create $WP_USER ${WP_USER_EMAIL} --user_pass=${WP_USER_PASS} --role=subscriber --display_name=${WP_USER} --porcelain --path=${WP_PATH} --allow-root
+	wp plugin install redis-cache --activate --allow-root
+	wp plugin update --all --allow-root
+	wp plugin activate redis-cache --allow-root
+	echo "define('WP_CACHE', true);" >> wp-config.php
+	echo "define('WP_REDIS_HOST', 'redis');" >> wp-config.php
+	echo "define('WP_REDIS_PORT', 6379);" >> wp-config.php
+	echo "define('WP_REDIS_TIMEOUT', 1);" >> wp-config.php
+	echo "define('WP_REDIS_READ_TIMEOUT', 1);" >> wp-config.php
+	echo "define('WP_REDIS_DATABASE', 0);" >> wp-config.php
 	echo "[ wordpress ] finished installation."
+	wp redis enable --force --allow-root
+	wp cache flush
+	sed -i "s|127.0.0.1|redis|g" wp-content/object-cache.php
 else
 	echo "[ wordpress ] already installed"
 fi
+
 
 exec /usr/sbin/php-fpm81 -F -R
